87a1164003b4f7167cf67b17b12aacc1fe0664c1
Samueljain26 <samueljain77@gmail.com>
2025-03-10 14:47:22 +0530

    [Samuel] ADD:Printin Mobile Phone details.

--- MobileDetails.java
+++ MobileDetails.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
-
-public class MobileDetails {
+import java.util.*;
+//MobilePhone class which has the objects
+class MobilePhone{
+    String brand;
+    String model;
+    int price;
+    //method to display
+    void display() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Price: " + price);
+    }
 }
+class MobileDetails{
+    //main method which takes input
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        //getting variable types from MobilePhone class
+        MobilePhone mobile =new MobilePhone();
+        System.out.print("Enter the Brand: ");
+        mobile.brand = sc.nextLine();
+        System.out.print("Enter the Model: ");
+        mobile.model = sc.nextLine();
+        System.out.print("Enter the price: ");
+        mobile.price = sc.nextInt();
+        //calling the display method to print output
+        mobile.display();
+    }
+}
+/*Enter the Brand: One plus
+Enter the Model: Nord
+Enter the price: 25000
+Brand: One plus
+Model: Nord
+Price: 25000
+ */
